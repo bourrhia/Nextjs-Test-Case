c1852056d6a58722b765db70b62631a1ca529095
Bourrhia El Mostafa <bourrhiamostafa@gmail.com>
2023-10-01 17:37:39 +0100

    initial : initial commit46

--- pages/api/generateUserId.js
+++ pages/api/generateUserId.js
@@ -12,9 +12,10 @@ export default async (req, res) => {
     const result = await db.collection("user").insertOne({});
 
     const generatedUserId = result.insertedId;
+    return res.status(200).json({ userId: generatedUserId });
 
     // Find the document by generated _id
-    const user = await db.collection("user").findOne({ _id: generatedUserId });
+    // const user = await db.collection("user").findOne({ _id: generatedUserId });
 
     /*if (user) {
       const userId = user.userId;
@@ -23,12 +24,12 @@ export default async (req, res) => {
       return null; // User with the given _id not found
     }*/
 
-    if (user && user.userId) {
+    /*if (user && user.userId) {
       const userId = user.userId;
       return res.status(200).json({ userId: userId });
     } else {
       return res.status(404).json({ message: "User Not Found" });
-    }
+    }*/
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ error: "Internal Server Error" });
